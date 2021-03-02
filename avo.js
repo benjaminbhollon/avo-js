110bddcf560c4091e0fafe55aea773831f77c536
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-03-03 00:15:38 +0800

    Rename variables

--- avo.js
+++ avo.js
@@ -1,23 +1,23 @@
-const initial = {
+const avoD = {
   callback: () => {},
   internalValue: undefined,
-  set value(value) {
-    this.internalValue = value;
+  set value(v) {
+    this.internalValue = v;
     this.callback();
   },
   get value() {
     return this.internalValue;
   },
-  bind: function bind(callback) {
-    if (typeof callback !== 'function') throw `Uncaught TypeError: Callback is not a function`;
-    this.callback = callback;
+  bind: function bind(c) {
+    if (typeof c !== 'function') throw `TypeError: callback is not a function`;
+    this.callback = c;
   }
 };
-let vars = {};
+let avoV = {};
 
-function avo(varName) {
-  if (typeof varName !== 'string' || varName.length === 0) throw `Invalid varName "${varName}"`;
-  if (vars[varName] === undefined) vars[varName] = initial;
+function avo(v) {
+  if (typeof v !== 'string') throw `Invalid varName "${v}"`;
+  if (avoV[v] === undefined) avoV[v] = avoD;
 
-  return vars[varName];
+  return avoV[v];
 }
